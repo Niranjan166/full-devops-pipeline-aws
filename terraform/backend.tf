6e5959f2c258a02a48fae937ab0e25278407b22a
Niranjan166 <niranjan01125@gmail.com>
2026-09-26 19:15:10 +0530

    testing staging

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,3 +7,23 @@ terraform {
     encrypt        = true
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "dms-nitesh-terraform-state"
+    key            = "staging/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "dms-terraform-lock"
+    encrypt        = true
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "dms-nitesh-terraform-state"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "dms-terraform-lock"
+    encrypt        = true
+  }
+}
